08dd1fad3d87d72409fabe4fcff04ff83e82398e
Jaimin Patel <jaimin@vertextechnology.com>
2020-06-08 12:18:20 +0530

    heroku deployment files are added

---
+++ setup.py
@@ -0,0 +1,17 @@
+def main():
+    # run initWorldCity.py file
+    print("...Running initWorldCity.py file...")
+    exec(open('initWorldCity.py').read())
+    print("...Sccesfully ran initWorldCity.py file...\ngi")
+
+    # run initWorldState.py file
+    print("...Running initWorldState.py file...")
+    exec(open('initWorldState.py').read())
+    print("...Sccesfully ran initWorldState.py file...")
+
+
+
+
+
+if __name__ == '__main__':
+    main()
